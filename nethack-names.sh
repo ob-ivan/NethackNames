20e94d0298ec3bbb44bf64dfdbf5e31db6607a82
Ob-Ivan <ob-ivan@users.noreply.github.com>
2018-01-17 01:20:58 +0300

    Generate gender and pass it to the name generator

--- nethack-names.sh
+++ nethack-names.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+function generate_gender {
+    case $(date +%N | tail -c2 | head -c1) in
+        0|2|4|6|8) echo male
+        1|3|5|7|9) echo female
+    esac
+}
+
 DIR=/var/games/nethack/save
 SAVE_COUNT=$(ls -1 $DIR | wc -l)
 SAVES=$(ls $DIR | sed -E 's/1000(.*).gz/\1/')
@@ -7,8 +15,9 @@ if [ $SAVE_COUNT -eq 1 ]; then
     NAME=$SAVES
 else
     if [[ -z $SAVES ]]; then
-        NAME=$(php nethack-names.php)
-        export NETHACKOPTIONS="catname:$(php nethack-names.php), dogname:$(php nethack-names.php), horsename:$(php nethack-names.php)"
+        GENDER=$(generate_gender)
+        NAME=$(php nethack-names.php gender=$GENDER)
+        export NETHACKOPTIONS="gender=$GENDER, catname:$(php nethack-names.php), dogname:$(php nethack-names.php), horsename:$(php nethack-names.php)"
     else
         echo 'Available names: ' $SAVES
         read -p 'Who are you? ' NAME
