c19781fda5c18d5ad280c219338b8709a38f11a9
Ob-Ivan <ob-ivan@users.noreply.github.com>
2018-01-16 23:34:22 +0300

    Extract the generator class to a separate file

--- nethack-names.php
+++ nethack-names.php
@@ -1,39 +1,5 @@
 <?php
-class NameGenerator {
-    const CONSONANTS = 'bcdfghjkllmmnnpqrssttvwxyz';
-    const VOWELS = 'aaeeiioouuy';
+require_once __DIR__ . '/src/NameGenerator.php';
 
-    public function generate(): string {
-        $length = 1 + rand(0, 1) + rand(0, 1) + rand(0, 1);
-        $syllables = [];
-        for ($i = 0; $i < $length; ++$i) {
-            $syllables[] = $this->generateSyllable();
-        }
-        return implode('', $syllables);
-    }
-
-    private function generateSyllable(): string {
-        $letters = [];
-        if (!rand(0, 1)) $letters[] = $this->generateConsonant();
-        if (!rand(0, 2)) $letters[] = $this->generateConsonant();
-        $letters[] = $this->generateVowel();
-        if (!rand(0, 2)) $letters[] = $this->generateConsonant();
-        if (!rand(0, 2)) $letters[] = $this->generateConsonant();
-        return implode('', $letters);
-    }
-
-    private function generateConsonant(): string {
-        return $this->getRandomChar(self::CONSONANTS);
-    }
-
-    private function generateVowel(): string {
-        return $this->getRandomChar(self::VOWELS);
-    }
-
-    private function getRandomChar(string $chars): string {
-        return $chars[rand(0, strlen($chars) - 1)];
-    }
-}
-
-$generator = new NameGenerator();
-echo ucfirst($generator->generate()) . "\n";
+$generator = new Ob_Ivan\NethackNames\NameGenerator();
+echo $generator->generate() . "\n";
