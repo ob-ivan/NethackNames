5a4270161269152b3aefd2d62680dd4f5e34f9c2
Ob-Ivan <ob-ivan@users.noreply.github.com>
2018-01-18 02:42:32 +0300

    Implement slavic name generator

--- src/GenderAwareInterface.php
+++ src/GenderAwareInterface.php
@@ -2,6 +2,9 @@
 namespace Ob_Ivan\NethackNames;
 
 interface GenderAwareInterface {
+    const GENDER_MALE   = 'male';
+    const GENDER_FEMALE = 'female';
+
     /**
      * Store gender for future usage.
      *
--- src/SlavicNameGenerator.php
+++ src/SlavicNameGenerator.php
@@ -2,6 +2,50 @@
 namespace Ob_Ivan\NethackNames;
 
 class SlavicNameGenerator implements GenderAwareInterface, NameGeneratorInterface {
+    const ROOTS = [
+        'bogu',
+        'bole',
+        'bori',
+        'broni',
+        'bryachi',
+        'daro',
+        'deye',
+        'dobro',
+        'dolgo',
+        'gordi',
+        'gore',
+        'gradi',
+        'hitro',
+        'isti',
+        'krasno',
+        'lado',
+        'mechi',
+        'milo',
+        'miro',
+        'ostro',
+        'pere',
+        'polku',
+        'rado',
+        'rosti',
+        'slavo',
+        'stani',
+        'sveto',
+        'svyato',
+        'tverdi',
+        'vence',
+        'verhu',
+        'vladi',
+        'vyache',
+        'vyshe',
+        'yaro',
+        'zharo',
+        'zhiro',
+        'zveni',
+        // mocking area
+        'kami', 'kaze',
+        'proto', 'popo',
+    ];
+
     private $gender;
 
     public function setGender(string $gender) {
@@ -10,14 +54,14 @@ class SlavicNameGenerator implements GenderAwareInterface, NameGeneratorInterfac
 
     public function generate(): string {
         $method = $this->select([
-            'generateDoubleRoot' => 1,
-            'generateSingleRoot' => 1,
+            'generateDoubleRoot' => 8,
+            'generateSingleRoot' => 2,
             'generateDiminutive' => 1,
         ]);
         return $this->$method();
     }
 
-    public function select(array $keyWeightMap) {
+    private function select(array $keyWeightMap) {
         $totalSum = array_reduce(
             $keyWeightMap,
             function ($sum, $item) { return $sum + $item; },
@@ -32,5 +76,31 @@ class SlavicNameGenerator implements GenderAwareInterface, NameGeneratorInterfac
             }
         }
         // Unreachable code.
+    }
+
+    private function generateDoubleRoot(): string {
+        return $this->getRandomRoot() . $this->generateSingleRoot();
+    }
+
+    private function generateSingleRoot(): string {
+        $name = mb_substr($this->getRandomRoot(), 0, -1);
+        if ($this->gender === GenderAwareInterface::GENDER_FEMALE) {
+            $name .= 'a';
+        }
+        return $name;
+    }
+
+    private function generateDiminutive(): string {
+        $root = mb_substr($this->getRandomRoot(), 0, -1);
+        if ($this->gender === GenderAwareInterface::GENDER_MALE) {
+            return $root . 'ik';
+        }
+        if ($this->gender === GenderAwareInterface::GENDER_FEMALE) {
+            return $root . 'ka';
+        }
+    }
+
+    private function getRandomRoot(): string {
+        return self::ROOTS[array_rand(self::ROOTS)];
     }
 }
